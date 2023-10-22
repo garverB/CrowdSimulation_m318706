07226ded324de15b9bb077f3659ce4bb6821ea58
bgv102 <m318706@alunos.ipb.pt>
2023-10-22 17:28:05 +0100

    Fourth step of the Flocking chapter

--- CrowdSimulation_m318706/Assets/Flock.cs
+++ CrowdSimulation_m318706/Assets/Flock.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Flock : MonoBehaviour
 {
     float speed;
+    bool turning = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,34 @@ public class Flock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Random.Range(0, 100) < 10)
+        Bounds b = new Bounds(FlockManager.FM.transform.position, FlockManager.FM.swimLimits * 2);
+        if(!b.Contains(transform.position))
         {
-            speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
+            turning = true;
         }
-        if (Random.Range(0, 100) < 10)
+        else
         {
-            ApplyRules();
+            turning = false;
+        }
+
+        if (turning)
+        {
+            Vector3 direction = FlockManager.FM.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(direction),
+                                                  FlockManager.FM.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+
+            if (Random.Range(0, 100) < 10)
+            {
+                speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
+            }
+            if (Random.Range(0, 100) < 10)
+            {
+                ApplyRules();
+            }
         }
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
